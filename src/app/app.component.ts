25f6e5bbb3f53ddcf341c087fc370f6ce8e72218
diegogomezlopez <diegogomezlopez91@gmail.com>
2021-04-06 19:44:46 +0200

    UPDATE app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { RoundResult } from './models/round-result';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rock-paper-scissors-frontend';
+  resultsTable:RoundResult[] = [];
+
+  constructor (private httpClient:HttpClient) {}
+
+  play() {
+    this.httpClient
+    .get<RoundResult>("http://localhost:8080/api/v1/playround")
+    .subscribe(data => {
+      this.resultsTable.push(data);
+    })
+  }
+
+  restart() {
+    this.resultsTable = [];
+  }
 }
