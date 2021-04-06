df326f2eb961e0ed2b5f5c2fa6b43ba92f82add7
diegogomezlopez <diegogomezlopez91@gmail.com>
2021-04-06 20:13:49 +0200

    UPDATE app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { RoundResult } from './models/round-result';
+import { RoundHistory } from './models/rounds-history';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rock-paper-scissors-frontend';
   resultsTable:RoundResult[] = [];
+  roundHistory:RoundHistory;
 
   constructor (private httpClient:HttpClient) {}
 
   play() {
     this.httpClient
-    .get<RoundResult>("http://localhost:8080/api/v1/playround")
-    .subscribe(data => {
-      this.resultsTable.push(data);
-    })
+        .get<RoundResult>("http://localhost:8080/api/v1/playround")
+        .subscribe(data => {
+          this.resultsTable.push(data);
+          this.getHistory();
+        })
+  }
+
+  private getHistory() {
+    this.httpClient
+        .get<RoundHistory>("http://localhost:8080/api/v1/history")
+        .subscribe(history => {
+          this.roundHistory = history;
+        })
   }
 
   restart() {
     this.resultsTable = [];
+  }
+
+  ngOnInit() {
+    this.getHistory();
   }
 }
